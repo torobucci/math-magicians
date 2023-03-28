ad70e2b3c4e0c8ed3bbd64f040540dea913a9a64
torobucci <kevintoro254@gmail.com>
2023-03-28 23:26:34 +0300

    make changes required

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import Calculator from './calculator';
+import Calculator from './components/Calculator';
 
 function App() {
   return (
--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,26 +1,10 @@
+import Buttons from './Buttons';
+
 function Calculator() {
   return (
     <div className="calculator">
       <input type="text" placeholder="0" className="input" />
-      <button type="button" className="button">A/C</button>
-      <button type="button" className="button">+/-</button>
-      <button type="button" className="button">%</button>
-      <button type="button" className="button operator">÷</button>
-      <button type="button" className="button">7</button>
-      <button type="button" className="button">8</button>
-      <button type="button" className="button">9</button>
-      <button type="button" className="button operator">x</button>
-      <button type="button" className="button">4</button>
-      <button type="button" className="button">5</button>
-      <button type="button" className="button">6</button>
-      <button type="button" className="button operator">-</button>
-      <button type="button" className="button">1</button>
-      <button type="button" className="button">2</button>
-      <button type="button" className="button">3</button>
-      <button type="button" className="button operator">+</button>
-      <button type="button" className="button zero">0</button>
-      <button type="button" className="button">.</button>
-      <button type="button" className="button operator">=</button>
+      <Buttons />
     </div>
   );
 }
