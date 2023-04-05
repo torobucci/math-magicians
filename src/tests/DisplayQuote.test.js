491970c2cc3d1032a85c61705c148057578866ac
torobucci <kevintoro254@gmail.com>
2023-04-06 00:40:10 +0300

    fix linter error

--- src/tests/DisplayQuote.test.js
+++ src/tests/DisplayQuote.test.js
@@ -5,18 +5,16 @@ import DisplayQuote from '../components/DisplayQuote';
 
 describe('display quote', () => {
   test('Should display loading text', async () => {
-    
     await waitFor(() => {
-        render(<DisplayQuote />)
-      });
+      render(<DisplayQuote />);
+    });
     await waitFor(() => {
       screen.getByText('Loading ...');
-      
     });
   });
   it('Render correctly', async () => {
     const quote = await act(async () => render(<DisplayQuote />));
-  
+
     expect(quote).toMatchSnapshot();
   });
 });
--- src/tests/NavBar.test.js
+++ src/tests/NavBar.test.js
@@ -1,10 +1,10 @@
 import renderer from 'react-test-renderer';
+import { BrowserRouter } from 'react-router-dom';
 import NavBar from '../components/NavBar';
-import { BrowserRouter} from 'react-router-dom';
 
 describe('NavBar', () => {
   test('Should render elements correctly', () => {
     const tree = renderer.create(<BrowserRouter><NavBar /></BrowserRouter>).toJSON();
     expect(tree).toMatchSnapshot();
   });
-})
+});
